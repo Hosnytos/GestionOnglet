a7946f0e21192c35b1851470d0b45bad332f766d
Hosnytos <hosnynaiya27@gmail.com>
2021-04-01 00:59:11 +0200

    TP4 before Exercice 1

--- app/src/main/java/com/example/gestiononglet/MainActivity.java
+++ app/src/main/java/com/example/gestiononglet/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
-        
+
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Icône message cliqué", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
